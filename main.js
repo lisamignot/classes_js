1435ba352fc91a13220e8e6f607d58b98cb83757
Lisa Mignot <lisammignot@gmail.com>
2024-11-02 00:38:12 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//criando classe
+class Bicicleta {
+    constructor(aro, pedal) {
+        this.aro = aro;
+        this.pedal = pedal;
+    }
+    
+}
+
+//primeira classe herdeira
+class Fixa extends Bicicleta {
+    constructor(aro, pedal, guidao) {
+        super(aro, pedal);
+        this.guidao = guidao;
+    }
+}
+
+//segunda classe herdeira
+class Single extends Bicicleta {
+    constructor(aro, pedal, marcha) {
+        super(aro, pedal);
+        this.marcha = marcha;
+    }
+}
+
+//instâncias de Bicicleta: Fixa e Single
+const caixinha = new Fixa(24, 'fixo', 'drop');
+const caloi = new Single(26, 'livre', true);
+const praiana = new Single(29, 'livre', false)
+
+
+console.log(caixinha);
+console.log(caloi);
+console.log(praiana);
+
+
+
